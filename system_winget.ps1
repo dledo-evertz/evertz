4b22f9cb128b3bf7c267b9a380a88ceb764c4255
Durval Ledo <dledo@evertz.com>
2024-05-24 11:32:37 -0400

    Created system_winget.ps1

---
+++ system_winget.ps1
@@ -0,0 +1,14 @@
+#region CODE
+$winget = $null
+$DesktopAppInstaller = "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+$SystemContext = Resolve-Path "$DesktopAppInstaller"
+if ($SystemContext) { $SystemContext = $SystemContext[-1].Path }
+$UserContext = Get-Command winget.exe -ErrorAction SilentlyContinue
+if ($UserContext) { $winget = $UserContext.Source }
+elseif (Test-Path "$SystemContext\AppInstallerCLI.exe") { $winget = "$SystemContext\AppInstallerCLI.exe" }
+elseif (Test-Path "$SystemContext\winget.exe") { $winget = "$SystemContext\winget.exe" }
+else { return $false }
+if ($null -ne $winget) { $winget }
+# Logs $(env:LOCALAPPDATA)\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\DiagOutputDir
+& "$winget" install --id "Microsoft.Teams" --source "winget" --exact --silent --accept-source-agreements --accept-package-agreements | Out-String
+#endregion
