4abd33380a8830fc64b2276ad1fba708d5a05d26
Durval Ledo <dledo@evertz.com>
2024-05-29 13:02:32 -0400

    testing different winget path

--- mk_winget.ps1
+++ mk_winget.ps1
@@ -2,17 +2,24 @@
 
 # Function to check if Winget is installed
 function Check-Winget {
-    $DesktopAppInstaller = "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
-    $SystemContext = Resolve-Path "$DesktopAppInstaller"
+    # $DesktopAppInstaller = "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+    # $SystemContext = Resolve-Path "$DesktopAppInstaller"
 
-    if ($SystemContext) { $SystemContext = $SystemContext[-1].Path }
+    # if ($SystemContext) { $SystemContext = $SystemContext[-1].Path }
 
-    $UserContext = Get-Command winget.exe -ErrorAction SilentlyContinue
+    # $UserContext = Get-Command winget.exe -ErrorAction SilentlyContinue
 
-    if ($UserContext) { $global:winget = $UserContext.Source }
-    #elseif (Test-Path "$SystemContext\AppInstallerCLI.exe") { $winget = "$SystemContext\AppInstallerCLI.exe" }
-    elseif (Test-Path "$SystemContext\winget.exe") { $winget = "$SystemContext\winget.exe" }
-    else { return $false }
+    # if ($UserContext) { $global:winget = $UserContext.Source }
+    # #elseif (Test-Path "$SystemContext\AppInstallerCLI.exe") { $winget = "$SystemContext\AppInstallerCLI.exe" }
+    # elseif (Test-Path "$SystemContext\winget.exe") { $winget = "$SystemContext\winget.exe" }
+    # else { return $false }
+
+    # hack
+    # get username
+    $username = get-wmiobject win32_computersystem | Select-Object username # EVERTZ_MICROSYS\username
+    $username = $username.username.ToString().Split('\')[-1]
+    Write-Output "username: $username"
+    $global:winget = "C:\Users\$username\AppData\Local\Microsoft\WindowsApps\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\winget.exe"
 
     if ($null -ne $global:winget) { 
         Write-Output "winget: $global:winget"
@@ -46,7 +53,7 @@ function Install-Apps {
 
     foreach ($App in $Apps) {
         try {
-            & $global:winget install --id $App --source "winget" --exact --silent --accept-source-agreements --accept-package-agreements --verbose | Out-String
+            & $global:winget install --id $App --source "winget" --exact --accept-source-agreements --accept-package-agreements --verbose
             Write-Output "$App has been installed."
         }
         catch {
