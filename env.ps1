e398c4ebed152cae8f9d81fc42a7c3c59ba4c161
Durval Ledo <dledo@evertz.com>
2024-05-29 09:19:12 -0400

    Changed env.ps1 to use local user instead of admin

--- env.ps1
+++ env.ps1
@@ -1,13 +1,23 @@
+# get username
+$username = get-wmiobject win32_computersystem | Select-Object username | Out-String # EVERTZ_MICROSYS\username
+
+$username = $username.ToString().Split('\')[-1]
+
+Write-Output "username: $username"
+
 Get-ChildItem env:* | Sort-Object name
 
 # Override env vars to mimic local Administrator account
-$env:APPDATA = "C:\Users\Administrator\AppData\Roaming"
-$env:LOCALAPPDATA = "C:\Users\Administrator\AppData\Local"
-$env:OneDrive = "C:\Users\Administrator\OneDrive"
-$env:TMP = "C:\Users\ADMINI~1\AppData\Local\Temp"
-$env:TEMP = "C:\Users\ADMINI~1\AppData\Local\Temp"
-$env:USERNAME = "Administrator"
-$env:USERPROFILE = "C:\Users\Administrator"
+$env:APPDATA = "C:\Users\$username\AppData\Roaming"
+$env:HOMEDRIVE = "C:"
+$env:HOMEPATH = "\Users\$username"
+$env:LANG = "en_US.UTF-8"
+$env:LOCALAPPDATA = "C:\Users\$username\AppData\Local"
+$env:OneDrive = "C:\Users\$username\OneDrive"
+$env:TMP = "C:\Users\$username\AppData\Local\Temp"
+$env:TEMP = "C:\Users\$username\AppData\Local\Temp"
+$env:USERNAME = "$username"
+$env:USERPROFILE = "C:\Users\$username"
 ""
 
 Get-ChildItem env:* | Sort-Object name
