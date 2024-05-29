3bff9440ae9fb314f6282fb3110f3a5e229407c1
Durval Ledo <dledo@evertz.com>
2024-05-29 10:58:18 -0400

    test old winget pathing

--- system_winget.ps1
+++ system_winget.ps1
@@ -20,18 +20,18 @@ $env:USERPROFILE = "C:\Users\$username"
 
 Get-ChildItem env:* | Sort-Object name
 
-# $winget = $null
-# $DesktopAppInstaller = "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
-# $SystemContext = Resolve-Path "$DesktopAppInstaller"
-# if ($SystemContext) { $SystemContext = $SystemContext[-1].Path }
-# $UserContext = Get-Command winget.exe -ErrorAction SilentlyContinue
-# if ($UserContext) { $winget = $UserContext.Source }
-# #elseif (Test-Path "$SystemContext\AppInstallerCLI.exe") { $winget = "$SystemContext\AppInstallerCLI.exe" }
-# elseif (Test-Path "$SystemContext\winget.exe") { $winget = "$SystemContext\winget.exe" }
-# else { return $false }
-# if ($null -ne $winget) { $winget }
-# # Logs $(env:LOCALAPPDATA)\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\DiagOutputDir
-$winget = "C:\Users\$username\AppData\Local\Microsoft\WindowsApps\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\winget.exe"
+$winget = $null
+$DesktopAppInstaller = "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+$SystemContext = Resolve-Path "$DesktopAppInstaller"
+if ($SystemContext) { $SystemContext = $SystemContext[-1].Path }
+$UserContext = Get-Command winget.exe -ErrorAction SilentlyContinue
+if ($UserContext) { $winget = $UserContext.Source }
+#elseif (Test-Path "$SystemContext\AppInstallerCLI.exe") { $winget = "$SystemContext\AppInstallerCLI.exe" }
+elseif (Test-Path "$SystemContext\winget.exe") { $winget = "$SystemContext\winget.exe" }
+else { return $false }
+if ($null -ne $winget) { $winget }
+# Logs $(env:LOCALAPPDATA)\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\DiagOutputDir
+# $winget = "C:\Users\$username\AppData\Local\Microsoft\WindowsApps\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\winget.exe"
 Write-Output "winget: $winget"
 & "$winget" --version
 & "$winget" install --id "Microsoft.Teams" --source "winget" --exact --silent --accept-source-agreements --accept-package-agreements --verbose | Out-String
