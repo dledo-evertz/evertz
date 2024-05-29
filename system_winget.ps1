8fa963f170dc2ebbc1dde36080adc02f5b5800a2
Durval Ledo <dledo@evertz.com>
2024-05-29 10:00:02 -0400

    Changed system_winget.ps1 to use local user instead of admin

--- system_winget.ps1
+++ system_winget.ps1
@@ -1,13 +1,22 @@
-#region CODE
+# get username
+$username = get-wmiobject win32_computersystem | Select-Object username | Out-String # EVERTZ_MICROSYS\username
+
+$username = $username.ToString().Split('\')[-1]
+
+Write-Output "username: $username"
 
 # Override env vars to mimic local Administrator account
-$env:APPDATA = "C:\Users\Administrator\AppData\Roaming"
-$env:LOCALAPPDATA = "C:\Users\Administrator\AppData\Local"
-$env:OneDrive = "C:\Users\Administrator\OneDrive"
-$env:TMP = "C:\Users\ADMINI~1\AppData\Local\Temp"
-$env:TEMP = "C:\Users\ADMINI~1\AppData\Local\Temp"
-$env:USERNAME = "Administrator"
-$env:USERPROFILE = "C:\Users\Administrator"
+$env:APPDATA = "C:\Users\$username\AppData\Roaming"
+$env:HOMEDRIVE = "C:"
+$env:HOMEPATH = "\Users\$username"
+$env:LANG = "en_US.UTF-8"
+$env:LOCALAPPDATA = "C:\Users\$username\AppData\Local"
+$env:OneDrive = "C:\Users\$username\OneDrive"
+$env:TMP = "C:\Users\$username\AppData\Local\Temp"
+$env:TEMP = "C:\Users\$username\AppData\Local\Temp"
+$env:USERNAME = "$username"
+$env:USERPROFILE = "C:\Users\$username"
+""
 
 $winget = $null
 $DesktopAppInstaller = "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
@@ -22,4 +31,3 @@ if ($null -ne $winget) { $winget }
 # Logs $(env:LOCALAPPDATA)\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\DiagOutputDir
 & "$winget" --version
 & "$winget" install --id "Microsoft.Teams" --source "winget" --exact --silent --accept-source-agreements --accept-package-agreements --verbose | Out-String
-#endregion
