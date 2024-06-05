c893cbe2fc49ff4a5be04092bad78f097facf205
Durval Ledo <dledo@evertz.com>
2024-06-05 09:41:05 -0400

    New latest version checking

--- install_latest_msteams.ps1
+++ install_latest_msteams.ps1
@@ -28,9 +28,11 @@ function InstallOrUpdateTeams {
         Write-Host "Microsoft Teams Windows App version $installedVersion is already installed."
 
         # Check for latest version
-        $wingetTeams = Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/winget-pkgs/contents/manifests/m/Microsoft/Teams"
-        $wingetTeamsLatestIndex = ($wingetTeams.Count - 3) # subtract 3 to avoid "classic" & "free" versions
-        $teamsLatestVersion = ($wingetTeams.item($wingetTeamsLatestIndex)).Name
+        # $wingetTeams = Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/winget-pkgs/contents/manifests/m/Microsoft/Teams"
+        # $wingetTeamsLatestIndex = ($wingetTeams.Count - 3) # subtract 3 to avoid "classic" & "free" versions
+        # $teamsLatestVersion = ($wingetTeams.item($wingetTeamsLatestIndex)).Name
+
+        $teamsLatestVersion = ((Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/winget-pkgs/commits?path=manifests/m/Microsoft/Teams&per_page=100").commit | Where-Object message -like "*Microsoft.Teams version*").message.split(" ")[4]
 
         if ($installedVersion -ne $teamsLatestVersion) {
             Write-Host "Microsoft Teams Windows App is outdated (Installed: $installedVersion Latest: $teamsLatestVersion). Updating..."
